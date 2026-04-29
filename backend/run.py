120638fa98adb8e2d15f9e6b2b1cc1ce3d01c41e
barbpic <cementbitengo19@gmail.com>
2026-04-29 14:02:37 +0300

    modified run.py

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = create_app()
 migrate = Migrate(app, db)
-CORS(app, origins=["https://genuine-begonia-092ff8.netlify.app"])
+CORS(app, supports_credentials=True, origins=["https://genuine-begonia-092ff8.netlify.app"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
